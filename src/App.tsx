1e139c7921ee9b4de9f216348c41489b323e76fe
MNewbigging <mrnewbigging@gmail.com>
2023-03-21 20:18:34 +0000

    threejs setup

--- src/App.tsx
+++ src/App.tsx
@@ -17,16 +17,7 @@ interface AppProps {
 export const App: React.FC<AppProps> = observer(({ appState }) => {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <h1>Vite + React</h1>
+      <canvas id="canvas"></canvas>
 
       <div className="card">
         <button onClick={appState.incrementCount}>
--- src/app-state.ts
+++ src/app-state.ts
@@ -1,13 +1,58 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { action, makeAutoObservable, observable } from "mobx";
 
 export class AppState {
   @observable count = 0;
 
+  private scene = new THREE.Scene();
+  private camera = new THREE.PerspectiveCamera(75, 1, 0.1, 100);
+  private renderer?: THREE.WebGLRenderer;
+  private controls?: OrbitControls;
+
   constructor() {
     makeAutoObservable(this);
+
+    // Give canvas time to mount
+    setTimeout(() => this.setupThree(), 1000);
   }
 
   @action incrementCount = () => {
     this.count++;
+  };
+
+  private setupThree() {
+    const canvas = document.getElementById("canvas");
+    if (!canvas) {
+      console.error("could not find canvas");
+      return;
+    }
+
+    this.renderer = new THREE.WebGLRenderer({ canvas });
+    this.renderer.setSize(640, 640);
+
+    this.scene.background = new THREE.Color("#1680AF");
+
+    this.controls = new OrbitControls(this.camera, canvas);
+    this.controls.enableDamping = true;
+
+    this.camera.position.z = 2;
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    this.scene.add(ambientLight);
+
+    const boxGeom = new THREE.BoxGeometry();
+    const boxMat = new THREE.MeshBasicMaterial({ color: "green" });
+    const box = new THREE.Mesh(boxGeom, boxMat);
+    this.scene.add(box);
+
+    this.update();
+  }
+
+  private update = () => {
+    requestAnimationFrame(this.update);
+
+    this.renderer?.render(this.scene, this.camera);
+    this.controls?.update();
   };
 }

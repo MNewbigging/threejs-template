486ab8382b4237f2c39b2f9db1eec1a827480806
MNewbigging <mrnewbigging@gmail.com>
2023-03-21 19:41:25 +0000

    showing images

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import "./app.scss";
 
 import React from "react";
+import planet00 from "/planet00.png";
 import viteLogo from "/vite.svg";
 import { observer } from "mobx-react-lite";
-import { useState } from "react";
 
+import planet08 from "./assets/planets/planet08.png";
 import reactLogo from "./assets/react.svg";
 import { AppState } from "./app-state";
 
@@ -23,49 +25,22 @@ export const App: React.FC<AppProps> = observer(({ appState }) => {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
+
       <div className="card">
         <button onClick={appState.incrementCount}>
           count is {appState.count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR is working
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="planets">
+        <div className="planet-7 logo"></div>
+        <img src={planet00} className="logo" />
+
+        <div className="planet-9 logo"></div>
+        <img src={planet08} className="logo" />
+      </div>
     </div>
   );
 });
-
-// function App() {
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <div className="App">
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://reactjs.org" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR is working
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </div>
-//   );
-// }
-
-export default App;

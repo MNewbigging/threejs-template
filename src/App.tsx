d38103efcaf148b073e8da6420124dc7b245597e
MNewbigging <mrnewbigging@gmail.com>
2023-03-30 16:47:43 +0100

    handle canvas resizing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import "./App.css";
 import "./app.scss";
 
 import React from "react";
-import planet00 from "/planet00.png";
-import viteLogo from "/vite.svg";
 import { observer } from "mobx-react-lite";
 
-import planet08 from "./assets/planets/planet08.png";
-import reactLogo from "./assets/react.svg";
 import { AppState } from "./app-state";
 
 interface AppProps {
@@ -16,22 +11,8 @@ interface AppProps {
 
 export const App: React.FC<AppProps> = observer(({ appState }) => {
   return (
-    <div className="App">
+    <div className="app">
       <canvas id="canvas"></canvas>
-
-      <div className="card">
-        <button onClick={appState.incrementCount}>
-          count is {appState.count}
-        </button>
-      </div>
-
-      <div className="planets">
-        <div className="planet-7 logo"></div>
-        <img src={planet00} className="logo" />
-
-        <div className="planet-9 logo"></div>
-        <img src={reactLogo} className="logo" />
-      </div>
     </div>
   );
 });
--- src/app-state.ts
+++ src/app-state.ts
@@ -1,69 +1,22 @@
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { action, makeAutoObservable, observable } from "mobx";
+
+import { GameState } from "./assets/game-state";
 
 export class AppState {
-  @observable count = 0;
-
-  private scene = new THREE.Scene();
-  private camera = new THREE.PerspectiveCamera(75, 1, 0.1, 100);
-  private renderer?: THREE.WebGLRenderer;
-  private controls?: OrbitControls;
+  gameState?: GameState;
 
   constructor() {
-    makeAutoObservable(this);
-
     // Give canvas time to mount
     setTimeout(() => this.setupThree(), 1000);
   }
 
-  @action incrementCount = () => {
-    this.count++;
-  };
-
   private setupThree() {
-    const canvas = document.getElementById("canvas");
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
     if (!canvas) {
       console.error("could not find canvas");
       return;
     }
 
-    this.renderer = new THREE.WebGLRenderer({ canvas });
-    this.renderer.setSize(640, 640);
-
-    this.scene.background = new THREE.Color("#1680AF");
-
-    this.controls = new OrbitControls(this.camera, canvas);
-    this.controls.enableDamping = true;
-
-    this.camera.position.z = 2;
-
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    this.scene.add(ambientLight);
-
-    const boxGeom = new THREE.BoxGeometry();
-    const boxMat = new THREE.MeshBasicMaterial({ color: "green" });
-    const box = new THREE.Mesh(boxGeom, boxMat);
-    this.scene.add(box);
-
-    const url = new URL("/chest.gltf", import.meta.url).href;
-    const loader = new GLTFLoader();
-    loader.load(url, (gltf) => {
-      const chest = gltf.scene;
-
-      chest.position.set(0, 0, 0);
-      this.scene.add(chest);
-      this.controls?.target.set(0, 0, 0);
-    });
-
-    this.update();
+    this.gameState = new GameState(canvas);
   }
-
-  private update = () => {
-    requestAnimationFrame(this.update);
-
-    this.renderer?.render(this.scene, this.camera);
-    this.controls?.update();
-  };
 }

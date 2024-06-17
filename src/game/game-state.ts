a532a9307b94bdcf3b5352f1a91a5e25152ff42d
Matt Newbigging <mrnewbigging@gmail.com>
2024-06-17 15:07:06 +0100

    removed first scene file

--- src/game/game-state.ts
+++ src/game/game-state.ts
@@ -2,14 +2,14 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 import { GameLoader } from "../loaders/game-loader";
-import { addGui } from "../utils/utils";
-import { FirstScene } from "./first-scene";
 
 export class GameState {
   private renderer: THREE.WebGLRenderer;
   private clock = new THREE.Clock();
 
-  private firstScene: FirstScene;
+  private scene = new THREE.Scene();
+  private camera = new THREE.PerspectiveCamera();
+  private controls: OrbitControls;
 
   constructor(private gameLoader: GameLoader) {
     // Setup renderer
@@ -25,11 +25,20 @@ export class GameState {
     const canvasRoot = document.getElementById("canvas-root");
     canvasRoot?.appendChild(canvas);
 
-    this.firstScene = new FirstScene(this.renderer, this.gameLoader);
 
     // Handle any canvas resize events
     window.addEventListener("resize", this.onCanvasResize);
     this.onCanvasResize();
+
+    this.setupCamera();
+    this.setupLights();
+    this.setupObjects();
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enableDamping = true;
+    this.controls.target.set(0, 1, 0);
+
+    this.scene.background = new THREE.Color("#1680AF");
 
     // Start game
     this.update();
@@ -37,22 +46,49 @@ export class GameState {
 
   private onCanvasResize = () => {
     const canvas = this.renderer.domElement;
-    const camera = this.firstScene.getCamera();
 
     this.renderer.setSize(canvas.clientWidth, canvas.clientHeight, false);
 
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    this.camera.aspect = canvas.clientWidth / canvas.clientHeight;
 
-    camera.updateProjectionMatrix();
+    this.camera.updateProjectionMatrix();
   };
+
+  private setupCamera() {
+    this.camera.fov = 75;
+    this.camera.far = 500;
+    const canvas = this.renderer.domElement;
+    this.camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    this.camera.position.set(0, 1.5, 3);
+  }
+
+  private setupLights() {
+    const ambientLight = new THREE.AmbientLight(undefined, 0.25);
+    this.scene.add(ambientLight);
+
+    const directLight = new THREE.DirectionalLight(undefined, Math.PI);
+    directLight.position.copy(new THREE.Vector3(0.75, 1, 0.75).normalize());
+    this.scene.add(directLight);
+  }
+
+  private setupObjects() {
+    const box = this.gameLoader.modelLoader.get("box");
+    this.scene.add(box);
+
+    const bandit = this.gameLoader.modelLoader.get("bandit");
+    bandit.position.z = -0.5;
+    this.scene.add(bandit);
+  }
 
   private update = () => {
     requestAnimationFrame(this.update);
 
     const dt = this.clock.getDelta();
 
-    this.firstScene.update(dt);
+    this.controls.update();
+
+    this.renderer.render(this.scene, this.camera)
   };
 }
